59340686eeec6000872c16d174fd18cef8377d8b
Mateusz <101126114+Luis-Sid-Frog@users.noreply.github.com>
2023-11-08 23:18:18 +0100

    #1 Managing game system (#2)
    * migrations and basic admin registration
    
    * fixtures for game system

--- rpg_app/admin.py
+++ rpg_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import GameSystem
+
 
 # Register your models here.
+
+class GameSystemAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+
+
+admin.site.register(GameSystem, GameSystemAdmin)
