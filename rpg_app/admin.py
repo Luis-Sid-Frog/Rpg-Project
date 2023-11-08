7aaca21f6808239ade27e96a561d727b4afa5fe2
Mateusz Sadowski <mateuszsadowski24@gmail.com>
2023-11-08 22:38:05 +0100

    pull request fix

--- rpg_app/admin.py
+++ rpg_app/admin.py
@@ -5,7 +5,7 @@ from .models import GameSystem
 # Register your models here.
 
 class GameSystemAdmin(admin.ModelAdmin):
-    list_display = ["name","description"]
+    list_display = ["name", "description"]
 
 
 admin.site.register(GameSystem, GameSystemAdmin)
