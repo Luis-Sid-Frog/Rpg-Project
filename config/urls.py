b68bbf321b225d403c77f539b5b553073709d318
Mateusz Sadowski <mateuszsadowski24@gmail.com>
2023-11-10 16:14:56 +0100

    3-define_basic_view_template_and_url_for_game_system_page

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("users/", include("rpg_project.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # # Your stuff: custom urls includes go here
-    path('rpg/', include('rpg_project.rpg_app.urls'))
+    path('', include('rpg_project.rpg_app.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn for local web socket development
