c60a7b97f77226497a7e753135c825cb5b91390c
Mateusz Sadowski <mateuszsadowski24@gmail.com>
2023-11-16 21:48:12 +0100

    Admin castomization

--- rpg_project/rpg_app/admin.py
+++ rpg_project/rpg_app/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import GameSystem, GameScenerio, Comment
 
 
+class GameSystemAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+
 admin.site.register(GameSystem)
 admin.site.register(GameScenerio)
 admin.site.register(Comment)
+
