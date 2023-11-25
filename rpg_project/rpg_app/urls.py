30ea4218288e27591559098ae8b564c5d1b9c17c
Mateusz Sadowski <mateuszsadowski24@gmail.com>
2023-11-25 14:56:07 +0100

    Adding CRUD do game scenerio

--- rpg_project/rpg_app/urls.py
+++ rpg_project/rpg_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', pages.home, name='home'),
     path('gamescenerio/<str:pk>/', pages.game_scenerio_page, name='game_scenerio'),
     path('createscenerio/', crud.create_scenerio, name='create-scenerio'),
+    path('updatecenerio/<str:pk>/', crud.update_game_scenerio, name='update-scenerio'),
+    path('deletecenerio/<str:pk>/', crud.delete_game_scenerio, name='delete-scenerio'),
+
 ]
--- rpg_project/rpg_app/views/crud.py
+++ rpg_project/rpg_app/views/crud.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rpg_project.rpg_app.models import GameSystem, GameScenerio, Comment
 from rpg_app.forms import GameScenerioForm
 
 def create_scenerio(request):
     form = GameScenerioForm()
+    if request.method == "POST":
+        form = GameScenerioForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+        else:
+            print('not working')
     context = {'form': form}
     return render(request, 'pages/scenerio_form.html', context)
+
+
+def update_game_scenerio(request, pk):
+    game_scenerio = GameScenerio.objects.get(id=pk)
+    form = GameScenerioForm(instance=game_scenerio)
+    if request.method == "POST":
+        form = GameScenerioForm(request.POST, instance=game_scenerio)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'pages/scenerio_form.html', context)
+
+
+def delete_game_scenerio(request, pk):
+    game_scenerio = GameScenerio.objects.get(id=pk)
+    if request.method == "POST":
+        game_scenerio.delete()
+        return redirect('home')
+    context = {'obj': game_scenerio}
+    return render(request, 'pages/delete_scenerio.html', context)
+
