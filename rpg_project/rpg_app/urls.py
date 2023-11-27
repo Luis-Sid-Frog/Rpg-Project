946a9b9922182abdc074f603b20e6db9c92fbaf6
Mateusz <101126114+Luis-Sid-Frog@users.noreply.github.com>
2023-11-27 22:15:35 +0100

    Feature/5 crud for game scenerio (#12)
    * Defining view,url and template for game system page
    
    * changed the position of rpg_app
    
    * 3-define_basic_view_template_and_url_for_game_system_page
    
    * Fixed bug with admin.py and models.py files
    
    * Fixed bug with admin.py and models.py files/2
    
    * Building home page and basic game scenerio page
    
    * Admin castomization
    
    * 4-creating_home_page_and_scenerio_create_form
    
    home_page_does_not_working
    
    * Adding CRUD do game scenerio
    
    * Creating_search_bar_,_game_scenerio_filter_by_game_system_and_viewing_adding_comments
    
    ---------
    
    Co-authored-by: Kacper Sieradzinski <ksieradzinski@gmail.com>

--- rpg_project/rpg_app/urls.py
+++ rpg_project/rpg_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . import views
+from .views import pages, crud
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('gamescenerio/<str:pk>/', views.game_system_page, name='game_scenerio'),
+    path('', pages.home, name='home'),
+    path('gamescenerio/<str:pk>/', pages.game_scenerio_page, name='game_scenerio'),
+    path('createscenerio/', crud.create_scenerio, name='create-scenerio'),
+    path('updatecenerio/<str:pk>/', crud.update_game_scenerio, name='update-scenerio'),
+    path('deletecenerio/<str:pk>/', crud.delete_game_scenerio, name='delete-scenerio'),
+
 ]
---
+++ rpg_project/rpg_app/views/pages.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from rpg_project.rpg_app.models import GameSystem, GameScenerio, Comment
+
+
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    game_scenerios = GameScenerio.objects.filter(game_system__name__icontains=q)
+    game_systems = GameSystem.objects.all()
+    comments = Comment.objects.all().order_by('-created')
+    context = {'game_scenerios': game_scenerios, 'game_systems':game_systems, 'comments':comments}
+    return render(request, 'pages/home.html', context)
+
+
+def game_scenerio_page(request, pk):
+    game_scenerio = GameScenerio.objects.get(id=pk)
+    comments = Comment.objects.all().order_by('-created')
+
+    if request.method =='POST':
+        comment = Comment.objects.create(
+            author = request.user,
+            game_scenerio = game_scenerio,
+            comment_body= request.POST.get('comment_body'),
+        )
+        return redirect('game_scenerio', pk=game_scenerio.id)
+    context = {'game_scenerio': game_scenerio, 'comments': comments}
+    return render(request, 'pages/gameScenerio.html', context)
